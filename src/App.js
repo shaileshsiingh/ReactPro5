a032dee4106276a5ebd99d61017e7c0671440e5e
Shailesh Singh <shaileshsiingh@gmail.com>
2023-08-10 18:03:10 +0530

    added a form to console log new movie inputs

--- src/App.js
+++ src/App.js
@@ -1,44 +1,103 @@
 import React, { useCallback, useEffect, useState } from 'react';
-
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const[movies, setMovies] = useState([])
-  const[isloading, setIsLoading] = useState(false)
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showAddForm, setShowAddForm] = useState(false); // To control showing the add movie form
+  const [newMovie, setNewMovie] = useState({
+    title: '',
+    openingText: '',
+    releaseDate: '',
+  });
 
-  
-  const fetchMoviesHandler = useCallback(async()=>{
-  setIsLoading(true)
-    const response = await fetch('https://swapi.dev/api/films')
-    const data = await response.json()
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData)=>{
-      return {
-        id:movieData.episode_id,
-        title:movieData.title,
-        openingText:movieData.opening_crawl,
-        releaseDate:movieData.release_date,
-      }
-    })
-    setMovies(transformedMovies)
-    setIsLoading(false)
-    console.log(transformedMovies)
-  },[])
-  
-  useEffect(()=>{
-    fetchMoviesHandler()
-  },[fetchMoviesHandler])
+    const transformedMovies = data.results.map((movieData) => ({
+      id: movieData.episode_id,
+      title: movieData.title,
+      openingText: movieData.opening_crawl,
+      releaseDate: movieData.release_date,
+    }));
+
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  const addMovieHandler = () => {
+    setShowAddForm(true);
+  };
+
+  const submitMovieHandler = () => {
+    const newMovieObj = {
+      id: movies.length + 1, // Replace this with a more appropriate way to generate IDs
+      ...newMovie,
+    };
+
+    setMovies((prevMovies) => [...prevMovies, newMovieObj]);
+    setShowAddForm(false);
+    console.log(newMovieObj);
+  };
 
   return (
     <React.Fragment>
-      <section>
+      <section> 
+         <button onClick={addMovieHandler}>Add Movie</button>
+</section>
+<section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isloading && <MoviesList movies={movies} />}
-        {isloading && <p>Loading...</p>}
+<section>
+        {showAddForm && (
+          <div>
+            <input
+              type="text"
+              placeholder="Title"
+              value={newMovie.title}
+              onChange={(event) =>
+                setNewMovie((prevMovie) => ({
+                  ...prevMovie,
+                  title: event.target.value,
+                }))
+              }
+            />
+            <input
+              type="text"
+              placeholder="Opening Text"
+              value={newMovie.openingText}
+              onChange={(event) =>
+                setNewMovie((prevMovie) => ({
+                  ...prevMovie,
+                  openingText: event.target.value,
+                }))
+              }
+            />
+            <input
+              type="text"
+              placeholder="Release Date"
+              value={newMovie.releaseDate}
+              onChange={(event) =>
+                setNewMovie((prevMovie) => ({
+                  ...prevMovie,
+                  releaseDate: event.target.value,
+                }))
+              }
+            />
+            <button onClick={submitMovieHandler}>Submit</button>
+          </div>
+        )}
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
+      
     </React.Fragment>
   );
 }
